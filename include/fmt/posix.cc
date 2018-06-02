d62c027e87222dfbef8451be858d6812622ac33c
Daniela Engert <dani@ngrt.de>
2018-06-02 16:17:50 +0200

    Visual Studio test projects added

---
+++ include/fmt/posix.cc
@@ -0,0 +1,1 @@
+../../src/posix.cc
---
+++ vs/include/LibraryCustomizations.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// libEnvironment
+#define NO_LIBENVIRONMENT_STARTUP
+#define NO_LIBENVIRONMENT_USERS
+#define NO_LIBENVIRONMENT_SETTINGS
+#define NO_LIBENVIRONMENT_IOBELEGUNG
+
+// Qt, Boost
+#pragma warning(disable: 4127) // conditional expression is constant
+#pragma warning(disable: 4714) // function marked as __forceinline not inlined
