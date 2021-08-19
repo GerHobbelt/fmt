125c34c5742a2d3ba7299b8faefa2a57e6ff639c
Patrick Fasano <patrick@patrickfasano.com>
2021-08-19 13:29:04 -0400

    submodule: Update module.mk

--- module.mk
+++ module.mk
@@ -4,9 +4,15 @@ $(eval $(begin-module))
 # unit definitions
 ################################################################
 
-module_units_h := core format-inl ostream printf time ranges
-module_units_cpp-h := format os  # note: renamed format.cc to format.cpp, etc., for use with makefile
+module_units_h := args chrono color compile core format format-inl
+module_units_h += locale os ostream printf ranges xchar
+
+# note: renamed format.cc to format.cpp, etc., for use with makefile
+module_units_cpp-h := format os
+# note: no C++20 module support in ndmakefile yet
+# module_units_cpp := fmt
 # module_units_f :=
+
 module_programs_cpp :=
 # module_programs_f :=
 # module_generated :=
