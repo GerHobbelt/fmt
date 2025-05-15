6b02d2fe6e242fa15aad7403c5ad1b11e7637bc5
tuxzz <dorazzsoft@gmail.com>
2025-05-15 17:34:10 +0800

    let it compiles on arm64ec clang-cl

--- include/fmt/format.h
+++ include/fmt/format.h
@@ -1387,7 +1387,7 @@ inline auto umul128(uint64_t x, uint64_t y) noexcept -> uint128_fallback {
 #if FMT_USE_INT128
   auto p = static_cast<uint128_opt>(x) * static_cast<uint128_opt>(y);
   return {static_cast<uint64_t>(p >> 64), static_cast<uint64_t>(p)};
-#elif defined(_MSC_VER) && defined(_M_X64)
+#elif defined(_MSC_VER) && !defined(_M_ARM64EC) && defined(_M_X64)
   auto hi = uint64_t();
   auto lo = _umul128(x, y, &hi);
   return {hi, lo};
